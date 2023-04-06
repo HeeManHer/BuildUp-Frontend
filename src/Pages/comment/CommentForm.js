ed6e6dac16b1cb1d638c9fcfeb7e723409f3c674
LJSUNG <ljsung6@naver.com>
2023-04-06 18:32:56 +0900

    Feature LJS/ISSUE

--- src/Pages/comment/CommentForm.js
+++ src/Pages/comment/CommentForm.js
@@ -1,50 +1,42 @@
-import { Component } from 'react'
+import React, { useState } from 'react'
 
-class CommentForm extends Component {
+function CommentForm(props) {
+    const [comment, setComment] = useState('')
 
-    state = {
-        value: ''
+    const handleChange = event => {
+        const value = event.target.value;
+        setComment(value)
     }
 
-    handleChange = e => {
-        const value = e.target.value
-        this.setState({ 
-            value, 
-        })
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        props.addList(comment);
+        setComment('');
     }
 
-    handleSubmit = e => {
-        e.preventDefault()
+    return (
+        <li className='comment-form'>
+            <form>
+                <span className='ps_box'>
+                    <input
+                        type='text'
+                        className='int'
+                        placeholder='댓글을 입력해주세요.'
+                        onChange={handleChange}
+                        value={comment}
+                    />
+                </span>
 
-        this.props.addList(this.state.value)
-        this.setState({
-            value: ''
-    })
-}
-
-    render() {
-        return(
-            <li className='comment-form'>
-                <form onSubmit={this.handleSubmit}> 
-                    <span className='ps_box'>
-                        <input  
-                        type='text' 
-                        className='int' 
-                        placeholder='댓글을 입력해주세요.' 
-                        onChange={this.handleChange}
-                        value={this.state.value}
-                        />
+                <button type='button' className="btn btn-success btn-icon-split btn-sm" onClick={handleSubmit}>
+                    <span className="icon text-white-50">
+                        <i className="fas fa-check"></i>
                     </span>
-
-                    <button type='submit' className="btn btn-success btn-icon-split btn-sm">
-                            <span className="icon text-white-50">
-                                <i className="fas fa-check"></i>
-                            </span>
-                            <span className="text">등록</span>
-                    </button>
-                </form>
-            </li>
-        )
-    }
+                    <span className="text">등록</span>
+                </button>
+            </form>
+        </li>
+    )
 }
-export default CommentForm
+
+export default CommentForm;
--- src/Pages/comment/Wan.js
+++ src/Pages/comment/Wan.js
@@ -1,48 +1,35 @@
-import { Component } from "react";
+import { useState } from "react";
 import Comment from "./Comment";
 import CommentForm from "./CommentForm";
 import CommentList from "./CommentList";
 
-class Wan extends Component {
 
-    state = {
-        value: '',
-        list:[
-            {userid:'', content:'', date: ''},
-        ]
-    }
+function Wan() {
+    // const [value, setValue] = useState('');
+    const [list, setList] = useState([{ userid: '', content: 'asd', date: '1' }]);
 
-    addList = (content) => {
-        const lastComment = this.state.list.length === 0 ? null : this.state.list[this.state.list.length - 1];
+    const addList = (content) => {
+        const lastComment = list.length === 0 ? null : list[list.length - 1];
         const newUserId = lastComment ? Number(lastComment.userid) + 1 : 1;
-        const today = new Date().toISOString().substr(0, 10); // 현재 날짜를 YYYY-MM-DD 형식으로 가져옴
-        this.setState({
-          list:[
-            ...this.state.list,
-            {userid: newUserId.toString(), content, date: today}
-          ]
-        })
-      }
+        const today = new Date().toISOString().substr(0, 10);
+        setList([
+            ...list,
+            { userid: newUserId, content, date: today }
+        ]);
+    };
 
-    updateList = list => {
-        this.setState({
-            ...this.state,
-            list,
-        })
-    }
+    const updateList = (updatedList) => {
+        setList(updatedList);
+    };
 
-    render() {
-        return(
-            <>
-                <Comment>
-                    <CommentForm addList = {this.addList}/>
-                    <CommentList list = { this.state.list }
-                                updateList = {this.updateList}
-                    />
-                </Comment>
-            </>
-        )
-    }
+    return (
+        <>
+            <Comment>
+                <CommentForm addList={addList} />
+                <CommentList list={list} updateList={updateList} />
+            </Comment>
+        </>
+    );
 }
 
-export default Wan
+export default Wan;
