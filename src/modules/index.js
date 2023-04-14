fe7a9ab0617ae1432e7055ff62cb10eb7a27a99b
LJSUNG <ljsung6@naver.com>
2023-04-14 09:54:04 +0900

    feature/issue

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,9 +2,10 @@ import { combineReducers } from "redux";
 import IssueReducer from "./Issue";
 import userReducer from "./user";
 import authorityReducer from "./authority";
+import BacklogReducer from "./Backlog";
 
 const rootReducer = combineReducers({
-    IssueReducer, userReducer, authorityReducer
+    IssueReducer, userReducer, authorityReducer, BacklogReducer
 });
 
 export default rootReducer;
