17397e99f5a49c8bbaa547c3c2e955f14cf86827
남효정 <namgywjd2@gmail.com>
2023-04-19 11:51:45 +0900

    [feature/project] 프론트

--- src/Pages/project/Manager.js
+++ src/Pages/project/Manager.js
@@ -1,86 +1,122 @@
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { NavLink, useParams } from 'react-router-dom';
-import userList from './User.json';
+import { useNavigate, useParams } from 'react-router-dom';
 import "../../css/project.css";
 
-import { getUserList, setUserList, changeUserList, deleteUserList } from '../../apis/UserListAPI';
-import { setAuthorityList } from '../../apis/AuthorityListAPI';
+import { addUserList, setUserList, changeUserList, deleteUserList, deleteProjectList, projectTitleEdit, authorityEdit } from '../../apis/UserListAPI';
 import { getProjectDetail } from '../../apis/NewprojectAPICalls';
 
 function Manager() {
+    
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    
+    const { projectNo } = useParams();
+    
     const [project, setProject] = useState('');
     const [deleteUser, setDeleteUser] = useState([]);
-    const [items, setItems] = useState([]);
-    const [title, setTitle] = useState('');
-    const [manager, setManager] = useState('');
     const [inviteText, setInviteText] = useState('');
     const [inviteList, setInviteList] = useState([]);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [selectedItemIndex, setSelectedItemIndex] = useState(-1);
-
-    const { projectNo } = useParams();
-
-
-    const handleInviteTextChange = (event) => {
-        setInviteText(event.target.value);
-    };
-
-    const handleInviteAdd = (e) => {
-        e.preventDefault();
-        setInviteList([...inviteList, inviteText]);
-        setInviteText("");
-    };
-
-    const handleOpenModal = () => {
-        setIsModalOpen(true);             //true일때 생성가능함
-    };
-
-    const handleCloseModal = () => {
-        setIsModalOpen(false);
-        setSelectedItemIndex(-1);         //false면 생성불가능함
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const newItem = {
-            title,
-            manager,
-            inviteText,
-        };
-
-        if (title && manager) { // 값이 모두 채워져 있는 경우에만 생성 가능
-            if (selectedItemIndex === -1) {
-                setItems([...items, newItem]);
-            } else {
-                const updatedItems = [...items];
-                updatedItems[selectedItemIndex] = newItem;
-                setItems(updatedItems);
-            }
-            setTitle('');
-            setManager('');
-            setInviteText('');
-            setInviteList([]);
-            handleCloseModal();
-        }
-    };
-
-    const handleInviteRemove = (index) => {
-        setInviteList(inviteList.filter(invite => inviteList.indexOf(invite) !== index));
-    };
+    const [edit, setEdit] = useState(false);
+    const [projectTitle, setProjectTitle] = useState('');
+    const [selected, setSelected] = useState([]);
 
     const userList = useSelector(state => state.userReducer);
-
-    const dispatch = useDispatch();
-
+    const selectList = userList.map((user) => ({roleNo: user.roleNo, roleName : user.roleName}));
+    const authority = userList.map((user) => ({roleNo: user.roleNo, roleName : user.role}));
+    
+    
     useEffect(
         () => {
             dispatch(setUserList(projectNo));
-            getProjectDetail(projectNo).then(data=> setProject(data));
+            getProjectDetail(projectNo).then(data => setProject(data));
         },
         []
-    );
-console.log(userList);
+    )
+
+    /* 권한 수정 버튼 */
+    const handlerSelect = (user, e) => {
+        setSelected(e.target.value);
+        authorityEdit(projectNo, user.employeeNo, e.target.value);
+    }
+
+    /* 프로젝트 명 수정 버튼 */
+    const onClickProjectTitleEditBtn = () => {
+        setEdit(true);        
+        setProjectTitle(project.projectTitle);
+    }
+    
+
+    /* input text로 바뀜 */
+    const handleInputChange = (e) => {
+        setProjectTitle(e.target.value);
+    }
+    
+
+    /* 프로젝트 명 저장 버튼 */
+    const handleProjectTitleUpdate = (e) => {
+        e.preventDefault();
+        setEdit(false);
+        projectTitleEdit(projectNo, projectTitle);
+        window.location.reload();
+    }
+
+
+    /* 프로젝트 삭제 버튼 */
+    const onClickProjectDeleteBtn = () => {
+
+        dispatch(deleteProjectList(projectNo));
+        navigate("../../../");
+    }
+    
+
+    /* 모달 열기(추가) 버튼 */
+    const handleOpenModal = () => {
+        setIsModalOpen(true);            
+    };
+
+
+    /* 모달 안에서 팀원 추가 버튼 */
+    const handleInviteAdd = () => {
+        if (inviteText == null || inviteText == "")
+            return;
+        setInviteList([...inviteList, { roleNo: 2, employeeName: inviteText }]);
+        setInviteText('');
+    }
+
+    
+    /* 팀원 초대에서 textChange */
+    const handleInviteTextChange = (event) => {
+        setInviteText(event.target.value);
+    }
+
+
+    /* 모달 안에서 팀원 초대한 사람 삭제 버튼 */
+    const handleInviteRemove = (index) => {
+        setInviteList(inviteList.filter(invite => inviteList.indexOf(invite) !== index));
+    }
+
+
+    /* 모달 안에서 맨 아래 추가 버튼 */
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        addUserList({ projectNo: projectNo, employeeName: inviteList }, projectNo);
+        setInviteList([]);
+        handleCloseModal();
+        window.location.reload();
+    }
+
+
+    /* 모달 안에서 맨 아래 취소 버튼 */
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+        setSelectedItemIndex(-1);         //false면 생성불가능함
+    }
+
+
+    /* 권한 수정 */
     const onChangeAuthority = (user, value) => {
 
         user = {
@@ -91,20 +127,26 @@ console.log(userList);
         dispatch(changeUserList(user));
     }
 
+
+    /* 사원 checkbox 버튼 */
     const onDeleteUserChecked = (userNo, isChecked) => {
         if (isChecked) {
             setDeleteUser([...deleteUser, userNo]);
         } else {
             setDeleteUser(deleteUser.filter(user => user !== userNo));
         }
-    };
+    }
 
+
+    /* 팀원 삭제 버튼 */
     const onClickDeleteBtn = () => {
 
-        dispatch(deleteUserList(deleteUser));
+        dispatch(deleteUserList(deleteUser, projectNo));
+        window.location.reload();
     }
-    return (
 
+
+    return (
         <div>
             {/* 모달스타일 적용 */}
             <div style={{ display: isModalOpen ? 'block' : 'none' }}>
@@ -132,12 +174,12 @@ console.log(userList);
                             <br />
                             <br />
                             <label className="title">
-                                제목 : 희만은 우리의 희망
+                                제목 : {project.projectTitle}
                             </label>
                             <br />
                             <br />
                             <label className="manager">
-                                담당자 : 허희만
+                                담당자 : {project.employeeName}
                             </label>
                             <br />
                             <br />
@@ -147,64 +189,60 @@ console.log(userList);
                                         팀원 초대 : <input
                                             type="text"
                                             value={inviteText}
-                                            onChange={(e) => setInviteText(e.target.value)}
-                                            style={{ width: "395px" }}
-                                        />
-                                        <button className="btn btn-success btn-icon-split btn-sm" type="button" onClick={handleInviteAdd}>
-                                            추가
-                                        </button>
+                                            onChange={handleInviteTextChange}
+                                            style={{ width: "395px" }} />
+                                        <button className="btn btn-success btn-icon-split btn-sm" type="button" onClick={handleInviteAdd}>추가</button>
                                     </label>
                                     <div className="row" style={{ overflow: "auto", height: "150px" }}>
                                         {inviteList.map((invite, index) => (
-                                            <div
-                                                key={index}
+                                            <div key={index}
                                                 style={{
                                                     border: "1px solid black",
                                                     padding: "5px",
                                                     margin: "5px",
                                                     display: "flex",
-                                                    height: "40px",
-                                                }}
-                                            >
-                                                <div>{invite}</div>
+                                                    height: "40px"
+                                                }}>
+                                                <div>{invite.employeeName}</div>
                                                 <button className='btn btn-danger btn-icon-split icon text-white fas fa-trash btn-sm'
                                                     type="button"
                                                     style={{ marginLeft: "20px" }}
-                                                    onClick={() => {
-                                                        setInviteList(inviteList.filter((_, i) => i !== index));
-                                                    }}
-                                                >
-                                                    삭제
-                                                </button>
-                                            </div>
+                                                    onClick={() => { handleInviteRemove(index) }}>삭제</button></div>
                                         ))}
                                     </div>
                                 </div>
-
                                 <br />
-
                             </div>
                             <button className='button2' type="submit" >{selectedItemIndex === -1 ? '추가' : 'Save'}</button>
-                            <button className="button3" type="button" onClick={() => handleCloseModal()}>{selectedItemIndex === -1 ? "취소" : "Cancel"}
-                            </button>
+                            <button className="button3" type="button" onClick={() => handleCloseModal()}>{selectedItemIndex === -1 ? "취소" : "Cancel"}</button>
                         </form>
                     </div>
                 </div>
             </div>
-
             <div className="newproject">
                 <h1>프로젝트 관리/<span id="size">{project.projectTitle}</span></h1>
-                <button className="button1">프로젝트 삭제</button>
+                <button className="button1" onClick={onClickProjectDeleteBtn} >프로젝트 삭제</button>
             </div>
             <hr className="line" />
-
             <div className="projectname">
-                <h3>프로젝트 명 : </h3>
-                <br />
-                <h6 id="name">{project.projectTitle}</h6>
+                {edit ? (
+                    <form onSubmit={handleProjectTitleUpdate}>
+                        <input type="text" value={projectTitle} onChange={handleInputChange} />
+                        <button className="button5" type="submit">수정</button>
+                    </form>
+                ) : (
+                    <>
+                        <h3>프로젝트 명 : </h3>
+                        <br />
+                        <h6 id="name">{project.projectTitle}</h6>
+                        <button className="button5" onClick={onClickProjectTitleEditBtn}>
+                            수정
+                        </button>
+                    </>
+                )}
             </div>
-            <br />
 
+            <br />
             <table className="user-table" width="90%">
                 <thead>
                     <tr>
@@ -222,48 +260,21 @@ console.log(userList);
                                 <input
                                     type="checkbox"
                                     name="delete"
-                                    id={user.no}
-                                    value={user.no}
+                                    id={user.employeeNo}
+                                    value={user.employeeNo}
                                     onClick={(e) =>
-                                        onDeleteUserChecked(e.target.value, e.target.checked)
-                                    }
+                                        onDeleteUserChecked(e.target.value, e.target.checked)}
                                 />
                             </td>
                             <td>{user.employeeName}</td>
                             <td>{user.employeeNo}</td>
                             <td>{user.employeeEmail}</td>
-                            <td>{user.roleName}
+                            <td><select onChange={(e) => handlerSelect(user,e)} value={user.roleNo}>
+                                {selectList.map((user) => (
+                                <option value={user.roleNo}>{user.roleName}
                                 
-                            </td>
-                        </tr>
-
-                    ))}
-                    {inviteList.map((invite, index) => (
-                        <tr key={index}>
-                            <td>
-                                <input
-                                    type="checkbox"
-                                    name="delete"
-                                    id={invite.no}
-                                    value={invite.no}
-                                    onClick={(e) =>
-                                        onDeleteUserChecked(e.target.value, e.target.checked)
-                                    }></input>
-                            </td>
-                            <td>{invite}</td>
-
-                            <td>{ }</td>
-                            <td>{ }</td>
-
-                            <td>
-                                <select name="authority"
-                                    value={invite.authority}
-                                    onChange={(e) => onChangeAuthority(invite, e.target.value)}>
-
-                                    <option value={invite.roleNo}>{invite.roleName}</option>
-                                    <option value="PM">PM</option>
-                                    <option value="부사수">부사수</option>
-                                    <option value="사수">사수</option>
+                                </option>
+                                ))}
                                 </select>
                             </td>
                         </tr>
@@ -273,14 +284,12 @@ console.log(userList);
             <br />
             <br />
             <div className="button">
-
                 <button type="button" className='button4' onClick={handleOpenModal}>추가</button>
                 <button type="button" className='button4'>저장</button>
                 <button type="button" className='button4'>취소</button>
                 <button type="button" className='button4' onClick={onClickDeleteBtn}>삭제</button>
             </div>
         </div>
-
     );
 }
 
