e3dd8ad60a2317a774b4ee544712fe0f3317a0d4
최명건 <choimyeonggeon@gmail.com>
2023-04-19 11:49:38 +0900

    feature/backlog
    수정본

--- src/modules/Backlog.js
+++ src/modules/Backlog.js
@@ -2,24 +2,34 @@ import { createActions, handleActions } from "redux-actions";
 
 
 /* 초기 state값 */
-const initialState = [
-
-];
+const initialState = {
+    data: [{}],
+    pageInfo: {}
+}
 
 /* 액션 타입 설정 */
-export const GET_BACKLOG = 'GET_BACKLOG';
+export const GET_BACKLOG = "GET_BACKLOG";
+export const DELETE_BACKLOG = "DELETE_BACKLOG";
+export const POST_BACKLOG = "POST_BACKLOG";
+export const PUT_BACKLOG = "PUT_BACKLOG";
 
 
 /*  액션 함수 */
 const actions = createActions({
-    [GET_BACKLOG]: () => { }
+    [GET_BACKLOG]: () => { },
+    [DELETE_BACKLOG]: () => { },
+    [POST_BACKLOG]: () => { },
+    [PUT_BACKLOG]: () => { },
 });
 
 
 /* 리듀서 함수 */
 const BacklogReducer = handleActions(
     {
-        [GET_BACKLOG]: (state, { payload }) => payload
+        [GET_BACKLOG]: (state, { payload }) => payload,
+        [DELETE_BACKLOG]: (state, { payload }) => payload,
+        [POST_BACKLOG]: (state, { payload }) => payload,
+        [PUT_BACKLOG]: (state, { payload }) => payload
     },
     initialState
 );
