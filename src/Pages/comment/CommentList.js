21a707da2d0313815b1ab904413a95d3485346e5
LJSUNG <ljsung6@naver.com>
2023-04-24 20:13:12 +0900

    feature/issue/ljs

--- src/Pages/comment/CommentList.js
+++ src/Pages/comment/CommentList.js
@@ -6,7 +6,7 @@ import "../../css/Comment.css";
 import { decodeJwt } from "../../utils/tokenUtils";
 
 //댓글 리스트 시작
-function CommentList({issueNo}) {
+function CommentList({ issueNo }) {
 
   const token = decodeJwt(window.localStorage.getItem("accessToken"));
 
@@ -19,10 +19,10 @@ function CommentList({issueNo}) {
 
   useEffect(
     () => {
-    dispatch(getComment(issueNo));
-  },
-   [comments]
-   );
+      dispatch(getComment(issueNo));
+    },
+    []
+  );
 
   // console.log(issueNo) 작동하는지 확인용
 
@@ -41,10 +41,10 @@ function CommentList({issueNo}) {
     if (event.key !== 'Enter') return;
 
     const modifyComment = comments[key];
-    
+
 
     setUpdate();
-    dispatch(updateComment({...modifyComment, replyContent: value}));
+    dispatch(updateComment({ ...modifyComment, replyContent: value }));
     dispatch(getComment(issueNo));
   };
 
@@ -58,45 +58,45 @@ function CommentList({issueNo}) {
   const rendList = () => {
     if (!comments) return null;
 
-    return comments.map((item, key) => 
-        <div className='comment-row' key={key}>
-          <div className='comment-id'> {item.employeeNo} </div>
-          <div className='comment-content'>
-            {/* 댓글 수정 */}
-            {update === key ? (
-              <input
-                type='text'
-                value={value}
-                onChange={handleChange}
-                onKeyDown={(event) => updateKeyDown(key, event)}
-                className='comment-update-input'
-              />
-            ) : (
-              <>
-                <span onClick={(event) => handleClick(key, event)}>{item.replyContent}</span>
-              </>
-            )}
-          
-          </div>
+    return comments.map((item, key) =>
+      <div className='comment-row' key={key}>
+        <div className='comment-id'> {item.employeeNo} </div>
+        <div className='comment-content'>
+          {/* 댓글 수정 */}
+          {update === key ? (
+            <input
+              type='text'
+              value={value}
+              onChange={handleChange}
+              onKeyDown={(event) => updateKeyDown(key, event)}
+              className='comment-update-input'
+            />
+          ) : (
+            <>
+              <span onClick={(event) => handleClick(key, event)}>{item.replyContent}</span>
+            </>
+          )}
 
-          {/* 댓글 날짜 */}
-          <div className='comment-date'>{new Intl.DateTimeFormat('kr').format(new Date(item.replyDate))}
-             {/* 댓글삭제 */}
-             {token.sub === item.employeeNo ?
-             <button
-                type="button"
-                  className='btn btn-danger btn-icon-split icon text-white-50 fas fa-trash btn-sm'
-                  onClick={()=>deleteList(item.replyNo)}
-                >
-                  삭제
-                </button>
-                :
-                <></>
-              }
-              </div>
         </div>
-        
-      );
+
+        {/* 댓글 날짜 */}
+        <div className='comment-date'>{new Intl.DateTimeFormat('kr').format(new Date(item.replyDate))}
+          {/* 댓글삭제 */}
+          {token.sub === item.employeeNo ?
+            <button
+              type="button"
+              className='btn btn-danger btn-icon-split icon text-white-50 fas fa-trash btn-sm'
+              onClick={() => deleteList(item.replyNo)}
+            >
+              삭제
+            </button>
+            :
+            <></>
+          }
+        </div>
+      </div>
+
+    );
   };
   return <div>{rendList()}</div>;
 }
